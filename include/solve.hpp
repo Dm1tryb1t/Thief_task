7ae66816876a2c0599dcc88ccd9933023ab0bf4c
Viacheslav <slava20082005@gmail.com>
2023-11-16 17:33:18 +0300

    cm

--- include/solve.hpp
+++ include/solve.hpp
@@ -2,4 +2,6 @@
 
 #include <vector>
 
-unsigned get_max_summary_value(const std::vector<Valuables>& vals, int max_weight, int cur_value, int cur_weight, int id);
+unsigned get_max_summary_value_recursive(const std::vector<Valuables>& vals, int max_weight, int cur_value, int cur_weight, int id);
+
+int get_max_summary_value_dynamic(const std::vector<Valuables>& vals, int max_weight);
